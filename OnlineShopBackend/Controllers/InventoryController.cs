a11aa8be5b3aff8351faf77aa687c4dc8919c25b
Shag10 <shubhangagrawal1999@gmail.com>
2026-03-28 03:33:48 +0530

    ADD - Added inventory Controller

---
+++ OnlineShopBackend/Controllers/InventoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace OnlineShopBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        public ActionResult SaveInventoryData()
+        {
+            SqlConnection sqlConnection = new SqlConnection
+            {
+                ConnectionString = "Server=(localdb)\\MSSQLLocalDB;Database=OnlineShoppingDB;"
+            };
+
+            SqlCommand sqlCommand = new SqlCommand
+            {
+                Connection = sqlConnection,
+                CommandText = "",
+                CommandType = CommandType.StoredProcedure,
+            };
+            sqlConnection.Open();
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+            return Ok("Inventory saved successfully.");
+        }
+    }
+}
